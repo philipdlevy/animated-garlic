244c7ee0d39ed0f09a881468bd0736bed1486ab4
Philip Levy <philipdlevy@gmail.com>
2022-01-20 19:04:17 -0700

    everything

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+
+
+
+
+
+
+
+
+
+/*
 const h2 = document.createElement("h2");
 h2.textContent.context = "This content added by JavaScript";
 
-document.querySelectory("body").appendChild(h2);
+document.querySelector("body").appendChild(h2);
+*/
